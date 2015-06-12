3d99aeffb8e8d6d3f0502a4752f9eb3bb4c5fc8e
AnnaErshova <anna.ershova@gmail.com>
2015-06-12 13:19:49 -0400

    further improves code

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,12 @@
 class Dog
 
-  attr_accessor :name, :breed,:leash, :plastic_bag, :walking, :vet, :vet_checkup
+  attr_accessor :name, :breed, :owner, :plastic_bag, :walking, :leash, :vet_checkup 
 
   # def initialize(name, breed, owner_name)
   def initialize(name, breed, owner_name)
     @name = name # this returns name of the dog
     @breed = breed # returns the breed classification
     @owner = Owner.new(owner_name, self) # returns Owner when #class is called on #owner 
-  end
-
-  def owner
-     @owner
   end
 
   def bark
